ea88dc30212ce6b0079bc894eccfc7b51bc99aeb
yux829 <yux829@sina.com>
2014-04-27 15:42:03 +0800

    t

---
+++ src/test/com/mopaas/sturgeon/dataparses/ParseTest.java
@@ -0,0 +1,37 @@
+package com.mopaas.sturgeon.dataparses;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import junit.framework.TestCase;
+
+public class ParseTest extends TestCase {
+ private final static String URL1="http://vip.stock.finance.sina.com.cn/usstock/balance.php?s=SINA&t=quarter";
+ private final static String URL2="http://money.finance.sina.com.cn/corp/go.php/vDOWN_CashFlow/displaytype/4/stockid/600000/ctrl/all.phtml";
+ 
+ public void testGet()  throws Exception{
+	 Parse p = new Parse();
+//	 Document doc= p.get(URL1);
+	 Document doc= p.get(URL2);
+	 System.out.println(doc.toString());
+//	 System.out.println("__________________________________________________\n");
+//	 Element elist=doc.getElementsByClass("data_tbl").get(1);
+//	 System.out.println(elist.toString());
+//	 System.out.println("__________________________________________________\n");
+		
+//	 elist=doc.getElementsByTag("table");
+//	 System.out.println(elist.toString());
+ }
+
+}
+
+class Parse {
+	public Document get(String url) throws IOException{
+		return Jsoup.connect(url).get();
+		
+	}
+}
