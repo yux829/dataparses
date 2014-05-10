0003706526832cdabc3b2b1807c8a277f59cda6e
yux829 <yux829@sina.com>
2014-05-10 23:33:25 +0800

    t

--- src/main/java/com/mopaas/sturgeon/dataparses/service/BalanceLoadDataService.java
+++ src/main/java/com/mopaas/sturgeon/dataparses/service/BalanceLoadDataService.java
@@ -1,42 +1,27 @@
 package com.mopaas.sturgeon.dataparses.service;
 
-import java.io.IOException;
+
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
-import junit.framework.Assert;
-
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.jsoup.Jsoup;
+import org.junit.Assert;
 import org.junit.Test;
 import org.nutz.castor.Castors;
-import org.nutz.castor.castor.String2Datetime;
 import org.nutz.json.Json;
+import org.springframework.stereotype.Service;
 
 import com.mopaas.sturgeon.dataparses.domain.BalanceDomain;
-
-public class BalanceLoadDataService {
+@Service
+public class BalanceLoadDataService extends BaseLoadDataService {
 	private final static String URL1 = "http://vip.stock.finance.sina.com.cn/usstock/balance.php?s=SINA&t=annual";
 	private final static String URL = "http://vip.stock.finance.sina.com.cn/usstock/balance.php?s=SINA&t=quarter";
 
-	private Document getBalanceContent(String URL) {
-		try {
-			return Jsoup.connect(URL).get();
-		} catch (IOException e) {
-			e.printStackTrace();
-
-		}
-		return null;
-
-	}
-
-	public List<BalanceDomain> parse(Document doc) {
-
+	
+	private List<BalanceDomain> parse(String URL) {
+		Document doc=this.getBalanceContent(URL);
 		Element table = doc.getElementsByClass("data_tbl").get(1);
-		// System.out.println(table.toString());
 		Elements trlist = table.getElementsByTag("tr");
 		Assert.assertEquals(43, trlist.size());
 		Element tr_curdate = trlist.get(0);
@@ -77,33 +62,31 @@ public class BalanceLoadDataService {
 			BalanceDomain bd = new BalanceDomain();
 			bd.setCurdate(Castors.me().castTo(curdates[i].substring(1),
 					java.util.Date.class));
-			bd.setCash_shortinvest(Castors.me().castTo(cash_shortinvests[i],Double.class));
-			bd.setReceivables(Castors.me().castTo(receivables[i],Double.class));
-			bd.setStock(Castors.me().castTo(stocks[i],Double.class));
-			bd.setAssets_current(Castors.me().castTo(assets_currents[i],Double.class));
-			bd.setAssets_total(Castors.me().castTo(assets_totals[i],Double.class));
-			bd.setDebts_current(Castors.me().castTo(debts_currents[i],Double.class));
-			bd.setDebts_total(Castors.me().castTo(debts_totals[i],Double.class));
-			bd.setShareholders(Castors.me().castTo(shareholders[i],Double.class));
-			bd.setStocks_numbers(Castors.me().castTo(stocks_numbers[i],Double.class));
+			bd.setCash_shortinvest(Castors.me().castTo(cash_shortinvests[i],
+					Double.class));
+			bd.setReceivables(Castors.me().castTo(receivables[i], Double.class));
+			bd.setStock(Castors.me().castTo(stocks[i], Double.class));
+			bd.setAssets_current(Castors.me().castTo(assets_currents[i],
+					Double.class));
+			bd.setAssets_total(Castors.me().castTo(assets_totals[i],
+					Double.class));
+			bd.setDebts_current(Castors.me().castTo(debts_currents[i],
+					Double.class));
+			bd.setDebts_total(Castors.me()
+					.castTo(debts_totals[i], Double.class));
+			bd.setShareholders(Castors.me().castTo(shareholders[i],
+					Double.class));
+			bd.setStocks_numbers(Castors.me().castTo(stocks_numbers[i],
+					Double.class));
 
 			blist.add(bd);
 		}
 		return blist;
 	}
 
-	private String[] parseTdContentFromTr(Element tr_content) {
-		StringBuffer sb = new StringBuffer("");
-		Elements td_list = tr_content.getElementsByTag("td");
-		for (Element td : td_list) {
-			sb.append(td.text()).append(";");
-		}
-		return sb.toString().split(";");
-	}
-
 	@Test
 	public void testparse() {
-		List<BalanceDomain> bdlist = this.parse(this.getBalanceContent(URL));
+		List<BalanceDomain> bdlist = this.parse(URL);
 		System.out.println(Json.toJson(bdlist));
 	}
 }
